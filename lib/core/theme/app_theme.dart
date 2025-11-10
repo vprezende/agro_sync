c63026888bdd3b757a84241ef7b2cc2997c08a82
Vinicius Rezende <vprezende.work@gmail.com>
2025-11-09 21:39:04 -0300

    ref: Organiza o tema da aplicação em componentes
    Este commit refatora a configuração de tema `ThemeData` para melhorar a organização e a modularidade. A estilização de componentes específicos foi extraída do arquivo principal `app_theme.dart` e movida para arquivos separados dentro de um novo diretório `lib/core/theme/components`.
    
    As principais alterações incluem:
    - Criação de arquivos de componentes de tema dedicados para: `AppBar`, `Drawer`, `FloatingActionButton` (FAB), `IconButton`, `TextButton`, `TextSelection`, e `InputDecoration`.
    - Remoção da configuração de tema inline do arquivo `app_theme.dart`, substituindo-a por referências aos novos componentes estáticos.
    - Atualização do arquivo `lib/core/theme/theme.dart` para incluir os novos arquivos de componentes como `part`.

--- lib/core/theme/app_theme.dart
+++ lib/core/theme/app_theme.dart
@@ -1,45 +1,14 @@
 part of theme;
 
 class AppTheme {
-
   static final lightThemeMode = ThemeData.light().copyWith(
     visualDensity: VisualDensity.adaptivePlatformDensity,
-    appBarTheme: AppBarTheme(
-      backgroundColor: AppPallete.transparent,
-    ),
-    drawerTheme: DrawerThemeData(
-      backgroundColor: AppPallete.grey300,
-    ),
-    floatingActionButtonTheme: FloatingActionButtonThemeData(
-      foregroundColor: AppPallete.white,
-      backgroundColor: AppPallete.blue,
-    ),
-    iconButtonTheme: IconButtonThemeData(
-      style: IconButton.styleFrom(
-        foregroundColor: AppPallete.white,
-      )
-    ),
-    textButtonTheme: TextButtonThemeData(
-      style: TextButton.styleFrom(
-        foregroundColor: AppPallete.white,
-        textStyle: AppTypography
-          .roboto
-          .regular
-          .base
-          .white
-      )
-    ),
-    textSelectionTheme: TextSelectionThemeData(
-      cursorColor: AppPallete.grey500_60p
-    ),
-    inputDecorationTheme: InputDecorationThemeData(
-      filled: true,
-      fillColor: AppPallete.white,
-      floatingLabelBehavior: FloatingLabelBehavior.never,
-      border: OutlineInputBorder(
-        borderRadius: BorderRadius.circular(8),
-        borderSide: BorderSide.none
-      ),
-    ),
+    appBarTheme: AppBarTheme.data,
+    drawerTheme: AppDrawerTheme.data,
+    floatingActionButtonTheme: AppFabTheme.data,
+    iconButtonTheme: AppIconButtonTheme.data,
+    textButtonTheme: AppTextButtonTheme.data,
+    textSelectionTheme: AppTextSelectionTheme.data,
+    inputDecorationTheme: AppInputTheme.data
   );
 }
---
+++ lib/core/theme/components/app_text_button_theme.dart
@@ -0,0 +1,14 @@
+part of theme;
+
+class AppTextButtonTheme {
+  static final data = TextButtonThemeData(
+    style: TextButton.styleFrom(
+      foregroundColor: AppPallete.white,
+      textStyle: AppTypography
+        .roboto
+        .regular
+        .base
+        .white
+    )
+  );
+}
